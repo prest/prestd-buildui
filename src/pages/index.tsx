08442116f33de69a987f3945d05e4b9171195142
Avelino <avelinorun@gmail.com>
2022-01-27 16:30:53 -0300

    home list all database
    Signed-off-by: Avelino <avelinorun@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,41 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faDatabase } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
-import { PRestTable } from "@postgresrest/node";
+import { PRestDatabase } from "@postgresrest/node";
 import { GetServerSideProps } from "next";
 import Link from "next/link";
 import React from "react";
 import Layout from "~/components/Layout";
-import { getDatabaseScheme, prestAPI } from "~/lib/prest";
+import { prestAPI } from "~/lib/prest";
 
 export type Props = {
-  tables: PRestTable[];
+  dbs: PRestDatabase[];
 };
 
-export const Home: React.FC<Props> = ({ tables = [] }) => (
+library.add(faDatabase);
+export const Databases: React.FC<Props> = ({ dbs = [] }) => (
   <Layout>
     <List>
-      {tables.length > 0 ? (
-        tables.map(({ name }) => (
-          <Link key={name} href={`/${name}`} passHref>
+      {dbs.length > 0 ? (
+        dbs.map(({ datname }) => (
+          <Link key={datname} href={`/${datname}`} passHref>
             <ListItem button component="a">
-              {name}
+              <FontAwesomeIcon icon="database" /> {datname}
             </ListItem>
           </Link>
         ))
       ) : (
-        <ListItem> Do not found any Tables </ListItem>
+        <ListItem> Do not found any Database </ListItem>
       )}
     </List>
   </Layout>
 );
 
 export const getServerSideProps: GetServerSideProps<Props> = async () => {
-  const tables = await prestAPI.tablesByDBInSchema(getDatabaseScheme);
-  return { props: { tables } };
+  const dbs = await prestAPI.databases();
+  return { props: { dbs } };
 };
 
-export default Home;
+export default Databases;
