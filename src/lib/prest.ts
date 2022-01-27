84a20c054a403c0b985bee69ec657a1a7c8c2d9d
Avelino <avelinorun@gmail.com>
2022-01-27 09:19:08 -0300

    read database/scheme env var
    Signed-off-by: Avelino <avelinorun@gmail.com>

--- src/lib/prest.ts
+++ src/lib/prest.ts
@@ -1,5 +1,8 @@
 import PRestAPI from "@postgresrest/node";
 
-export default new PRestAPI({
+export const getDatabaseScheme: string =
+  process.env.PREST_DATABASE + "." + process.env.PREST_SCHEME;
+
+export const prestAPI = new PRestAPI({
   baseUrl: process.env.PREST_URL,
 });
