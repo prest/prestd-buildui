f78ba8e5d2217dfda6b13099f84c5e2e5b22f9d9
Avelino <avelinorun@gmail.com>
2022-04-15 18:03:20 -0300

    init/db: populate example table

--- etc/postgres/000-initial.sql
+++ etc/postgres/000-initial.sql
@@ -7,3 +7,10 @@ CREATE TABLE IF NOT EXISTS example_table (
     created_at timestamp without time zone NOT NULL DEFAULT now(),
     PRIMARY KEY (id)
 );
+
+INSERT INTO example_table
+  (field_char_50_unique, email, field_text, field_type_jsonb)
+VALUES
+  ('unique 50 - 01', 'opensource@prestd.com', 'text field', '{"field1": "value field 1", "field2": "value field 2", "int_field": 10}'),
+  ('unique 50 - 02', 'open-source@prestd.com', 'text field', '{"field1": "value field 1", "field2": "value field 2", "int_field": 11}'),
+  ('unique 50 - 03', 'buildui@prestd.com', 'text field', '{"field1": "value field 1", "field2": "value field 2", "int_field": 12}');
